ef5acdb54fe65d22b1224f0e4fc714879f1d84e2
GustavoAlves <gustavo_rodrigo@id.uff.br>
2023-03-29 17:16:34 -0300

    colocando o ar

--- serve.js
+++ serve.js
@@ -1,14 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const nomeApp = process.env.npm_package_name;
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front-api-marvel'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front-api-marvel/'}),
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`)),
 );
 
 // Start the app by listening on the default Heroku port
